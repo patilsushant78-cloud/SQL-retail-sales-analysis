ffef95dea9645df71cf90b3154132ce67e5d01ee
Sushant Patil <patilsushant78@gmail.com>
2026-02-13 16:17:10 +0530

    schema.sql

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE orders (
+    row_id INT,
+    order_id VARCHAR(20),
+    order_date DATE,
+    ship_date DATE,
+    ship_mode VARCHAR(20),
+    customer_id VARCHAR(20),
+    customer_name VARCHAR(100),
+    segment VARCHAR(50),
+    country VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    postal_code VARCHAR(20),
+    region VARCHAR(50),
+    product_id VARCHAR(20),
+    category VARCHAR(50),
+    sub_category VARCHAR(50),
+    product_name VARCHAR(200),
+    sales DECIMAL(10,2),
+    quantity INT,
+    discount DECIMAL(5,2),
+    profit DECIMAL(10,2)
+);
